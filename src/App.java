159b0f12733997d2a0b44b3274b9cf7d72d6007a
felipe0402 <felipebaiano04@gmail.com>
2024-03-06 02:09:42 -0300

    Add formula celsius para Kelvi

--- src/App.java
+++ src/App.java
@@ -7,7 +7,8 @@ public class App {
         //Exibir as opçoes para o usuario
         System.out.println("Escolha uma opção");
         System.out.println("1. Coverter celsius para fahrenheit" );
-        System.out.println("2. Coverter fahrenheit para celsius");
+        System.out.println("2. Coverter celsius para Kelvin ");
+        System.out.println("3. Coverter fahrenheit para celsius");
         System.out.print("Opçao:");
 
         // Ler a opção escolhida pelo usuário
@@ -17,13 +18,21 @@ public class App {
        if (opcao == 1){
         System.out.println("Valor celsius:" );
         double celsius= s.nextInt();
-        System.out.println("A conversão de Graus para Fahrenhel:" +  ((celsius * 1.8) + 32));
+        System.out.println("Conversão de celsius para Fahrenhel:" +  ((celsius * 1.8) + 32));
+
        } else if  (opcao == 2){
+        System.out.println("Valor celsius:" );
+        double Kelvin= s.nextInt();
+        System.out.println("Cconversão de celsius para Kelvin:" + (Kelvin + 273.15));
+
+       } else if  (opcao == 3){
         System.out.println("Valor Fahrenhel:" );
         double fahrenhel= s.nextInt();
-        System.out.println("A conversão de Graus para Fahrenhel:" + ((fahrenhel - 32) /1.8));
-       } else {
-        System.out.println("Opção invalida ");
+        System.out.println("Conversão de Fahrenhel para celsius:" + ((fahrenhel - 32) /1.8));
+      
+       }
+        else {
+        System.out.println("Opcao invalida ");
        };
-    };
-};
+    }
+}
